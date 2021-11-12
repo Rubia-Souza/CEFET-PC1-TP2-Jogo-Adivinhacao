0351a8d1ad6521a5f78469666e1f22c70e7073a4
Rubia Souza <rubensvictor.m.s@gmail.com>
2021-11-12 09:18:40 -0300

    feat(save): Arruma a leitura do arquivo

--- src/saveSystem/saveSystem.c
+++ src/saveSystem/saveSystem.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 
@@ -10,24 +12,44 @@
 void createSaveFile();
 void createSaveFolder();
 
+const char SPLIT_CHARACTER = '|';
+
 void saveScore(const int playerScore, const int iaScore) {
     FILE *fileRef = fopen(SCORE_SAVE_FULL_FILE_PATH, "w");
 
-    fprintf(fileRef, "Histórico de potuação: Usuário: %d, IA: %d\n", playerScore, iaScore);
+    fprintf(fileRef, "Histórico de potuação: Usuário: %d %c IA: %d\n", playerScore, SPLIT_CHARACTER, iaScore);
     fclose(fileRef);
     
     return;
 }
 
 void loadScore(int *playerScore, int *iaScore) {
+    char actualCharacter = 'a';
+    char fileUserScore[100], fileIaScore[100];
+    char *target = fileUserScore;
+    int fillIndex = 0;
+    bool foundSplitCharacter = false;
     FILE *fileRef = fopen(SCORE_SAVE_FULL_FILE_PATH, "r");
-    int fileUserScore = 0, fileIaScore = 0;
 
-    fscanf(fileRef, "%d %d", &fileUserScore, &fileIaScore);
+    while(actualCharacter != EOF) {
+        actualCharacter = fgetc(fileRef);
+
+        foundSplitCharacter = actualCharacter == SPLIT_CHARACTER;
+        if (foundSplitCharacter) {
+            target = fileIaScore;
+            fillIndex = 0;
+        }
+
+        if (isdigit(actualCharacter)) {
+            target[fillIndex] = actualCharacter;
+            fillIndex++;
+        }
+    }
+
     fclose(fileRef);
 
-    *playerScore = fileUserScore;
-    *iaScore = fileIaScore;
+    *playerScore = atoi(fileUserScore);
+    *iaScore = atoi(fileIaScore);
 
     return;
 }
